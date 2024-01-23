60e0ac7834bf9826b82884d3534df366b61247ea
Itihell Mejia <itihell.mejia@gmail.com>
2024-01-23 16:57:30 -0600

    Creando la paginacion

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,5 @@
 import { getPaginatedProductsWithImages } from "@/actions";
-import { ProductGrid, Title } from "@/components";
+import { Pagination, ProductGrid, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 import { redirect } from "next/navigation";
 
@@ -24,6 +24,8 @@ export default async function Home({ searchParams }: Props) {
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
       <ProductGrid products={products} />
+
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
