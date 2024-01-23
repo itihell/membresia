595f9ae221b44613ecf489085ab1a641a443a21c
Itihell Mejia <itihell.mejia@gmail.com>
2024-01-23 11:51:00 -0600

    Obteniendo el total de paginas para la paginacion

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -15,6 +15,7 @@ export const getPaginatedProductsWithImages = async ({
     if (isNaN(Number(page))) page = 1;
     if (page < 1) page = 1;
 
+    // TODO: Obtener los productos con sus imagenes
     const products = await prisma.product.findMany({
       take: take,
       skip: (page - 1) * take,
@@ -28,9 +29,13 @@ export const getPaginatedProductsWithImages = async ({
       },
     });
 
+    // TODO: Obtener el total de paginas
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
+
     return {
-      currentPage: 1,
-      totalPages: 10,
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map((product) => ({
         ...product,
         images: product.ProductImage.map((image) => image.url),
