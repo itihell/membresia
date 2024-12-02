b53b52ceb9c5d08a2c1c42655ebed11091f72966
Itihell Mejia <itihell.mejia@gmail.com>
2024-12-02 17:06:49 -0600

    Quintando variables no usadas en tipos enventes interface

--- src/interfaces/tipo-eventos.interface.ts
+++ src/interfaces/tipo-eventos.interface.ts
@@ -1,7 +1,4 @@
 import { Evento } from "./eventos.interface";
-import { Iglesia } from "./iglesia.interface";
-import { User } from "./user.interface";
-
 export interface TipoEventos {
   id?: number;
   name: string;
